168cc4c9b03b81e5fc6fa6e24e9eff57cf1382eb
Ronnie Feng <ronnie@ronniefeng.com>
2021-01-12 15:57:14 -0500

    main section complete, style adjustments on the hero

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import {Nav} from "../components/nav";
 import {useScroll} from "../hooks/useScroll";
-
+import hoverEffect from "hover-effect";
+import Ronnie from "../assets/images/homepage/profile.jpg";
+import Ronnie2 from "../assets/images/homepage/profile2.jpeg";
+import HeatMap from "../assets/images/homepage/heightMap.png";
 
 const AboutStyles = styled.div`
-    padding: 6vw 4vw;
+    padding: 0 4vw;
     .big-text{
         height:100vh;
+        display: flex;
+        flex-wrap: wrap;
+        align-content: center;
+        margin-bottom: 10vh;
         h1{
             text-transform: uppercase;
             font-size: 11vw;
@@ -15,6 +22,7 @@ const AboutStyles = styled.div`
             height: 9vw;
             overflow: hidden;
             :nth-child(2){
+                flex-grow: 1;
                 text-align: right;
             }
             :nth-child(3){
@@ -47,10 +55,42 @@ const AboutStyles = styled.div`
             z-index: 5;
         }
     }
+    .main{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &-pic{
+            width: 25vw;
+            height: 30vw;
+            margin-right: 5vw;
+            text-align: center;
+        }
+        &-text{
+            width: 40%;
+            p{
+                margin: 3vw 0;
+                a{
+                    font-family: Jakarta;
+                    text-transform: none;
+                    mix-blend-mode: difference;
+                }
+            }
+        }
+    }
 `
 
 const About = ()=>{
     const {scrollRef} = useScroll();
+    const profileRef = useRef(null);
+    useEffect(()=>{
+        new hoverEffect({
+            parent: profileRef.current,
+            intensity: 1.4,
+            image1: Ronnie2,
+            image2: Ronnie,
+            displacementImage: HeatMap
+        })
+});
     return (
         <>
             <Nav/>
@@ -70,9 +110,12 @@ const About = ()=>{
                     <h1 data-scroll data-scroll-speed="0.5">Feng</h1>
                 </div>
                 <div data-scroll-section className="main">
-                    <div className="main-pic"></div>
+                    <div data-scroll data-scroll-speed="6" className="main-pic" ref={profileRef}></div>
                     <div className="main-text">
-                        <p></p>
+                        <p>For whatever reason that brought you to my portfolio, welcome. I hope you enjoyed browsing this site as much as I enjoyed building it. First, please allow me to introduce myself. </p>
+                        <p>I'm currently a graduate student at the University of Toronto. Prior to that, I completed my bachelor's degree at Boston University studying communications and American Studies. I worked in advertising for a bit and loved everything about it. I was very fortunate to participate in a few projects that served some notable clients, including <a href="https://www.dunkindonuts.com/en" target="_blank" rel="noreferrer">Dunkin Donuts</a>, <a href="https://www.nhl.com/bruins" target="_blank" rel="noreferrer">Boston Bruins</a>, <a href="https://www.partycity.ca/en.html" target="_blank" rel="noreferrer">Party City</a>, and <a href="https://fostergrant.com/" target="_blank" rel="noreferrer">Foster Grant</a>.</p>
+                        <p>I was introduced to coding in the fall of 2019 when I took a graduate python course at school. Soon after, I made the decision to transition into software development. Web, as a platform, allows me to harness on my experience in advertising and stretch my creative muscles. After about 10 months of grinding and teaching myself, I completed this website. No bootcamp, no CS degree, but the drive to achieve nothing less than excellence. No days off, I'm serious. </p>
+                        <p>In my spare time, I follow a number of boxers in the lower weight divisions. I've been a fervent fan of boxing for years. Gotta love the sweet science.</p>
                     </div>
                 </div>
 
