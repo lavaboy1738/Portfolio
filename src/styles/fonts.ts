e936b245338d27900b90dbd1d41dbb402e11bfac
Ronnie Feng <ronnie@ronniefeng.com>
2021-01-15 14:41:02 -0500

    getting rid of unused fonts

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,26 +1,10 @@
 import {css} from "styled-components";
-
-import JakartaRegularWoff from "../fonts/PlusJakartaSans-Regular.woff";
-import JakartaRegularWoff2 from "../fonts/PlusJakartaSans-Regular.woff2";
-import JakartaMediumWoff from "../fonts/PlusJakartaSans-Medium.woff";
-import JakartaMediumWoff2 from "../fonts/PlusJakartaSans-Medium.woff2";
-import JakartaBoldWoff  from "../fonts/PlusJakartaSans-Bold.woff";
-import JakartaBoldWoff2 from "../fonts/PlusJakartaSans-Bold.woff2";
-import JakartaExtraBoldWoff from "../fonts/PlusJakartaSans-ExtraBold.woff";
-import JakartaExtraBoldWoff2 from "../fonts/PlusJakartaSans-ExtraBold.woff2";
 import JakartaLightWoff from "../fonts/PlusJakartaSans-Light.woff";
 import JakartaLightWoff2 from  "../fonts/PlusJakartaSans-Light.woff2";
-import JakartaExtraLightWoff from "../fonts/PlusJakartaSans-ExtraLight.woff";
-import JakartaExtraLightWoff2 from "../fonts/PlusJakartaSans-ExtraLight.woff2";
 import Monument from "../fonts/MonumentExtended-Bold.otf";
 
 const jakartaNormalWeights = {
-    100: [JakartaExtraLightWoff, JakartaExtraLightWoff2],
     200: [JakartaLightWoff, JakartaLightWoff2],
-    400: [JakartaRegularWoff, JakartaRegularWoff2],
-    500: [JakartaMediumWoff, JakartaMediumWoff2],
-    600: [JakartaBoldWoff, JakartaBoldWoff2],
-    800: [JakartaExtraBoldWoff, JakartaExtraBoldWoff2]
 }
 
 
