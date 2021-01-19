4784844059246855aea107ae306922fb37f804f0
Ronnie Feng <ronnie@ronniefeng.com>
2021-01-19 11:37:00 -0500

    adjustments on the panels

--- src/components/panels.tsx
+++ src/components/panels.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import styled from "styled-components";
 import {motion} from "framer-motion";
 import {Animations} from "../animations";
