66e48fec05909507bd882bd1babc9709fa0164c8
Ronnie Feng <ronnie@ronniefeng.com>
2021-02-05 12:30:37 -0500

    adding animations

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,7 @@ import hoverEffect from "hover-effect";
 import Ronnie from "../assets/images/homepage/profile.jpg";
 import Ronnie2 from "../assets/images/homepage/profile2.jpeg";
 import DisplacementImg from "../assets/images/homepage/glass.jpg";
+import {Animations} from "../animations";
 import {motion} from "framer-motion";
 import {Panels} from "../components/panels";
 
@@ -36,6 +37,7 @@ const HomepageStyles = styled(motion.div)`
             }
         }
         &-bottom{
+            overflow: hidden;
             a{
                 margin-right: 3vw;
                 font-family: Grotesk;
@@ -55,17 +57,20 @@ const HomepageStyles = styled(motion.div)`
         }
         &-top{
             text-align: right;
-            h1{
-                font-size:calc(var(--VW)*13);
-                line-height: calc(var(--VW)*11);
-                height: calc(var(--VW)*11);
-                text-transform: uppercase;
-                -webkit-text-fill-color: var(--burlywood);
-                -webkit-text-stroke: calc(var(--VW)*0.14) var(--black);
-                @media (hover: none) and (pointer: coarse){
-                    font-size:calc(var(--VW)*17);
-                    line-height: calc(var(--VW)*16);
-                    height: calc(var(--VW)*16);
+            .name-wrapper{
+                overflow: hidden;
+                h1{
+                    font-size:calc(var(--VW)*13);
+                    line-height: calc(var(--VW)*11);
+                    height: calc(var(--VW)*11);
+                    text-transform: uppercase;
+                    -webkit-text-fill-color: var(--burlywood);
+                    -webkit-text-stroke: calc(var(--VW)*0.14) var(--black);
+                    @media (hover: none) and (pointer: coarse){
+                        font-size:calc(var(--VW)*17);
+                        line-height: calc(var(--VW)*16);
+                        height: calc(var(--VW)*16);
+                    }
                 }
             }
         }
@@ -90,7 +95,7 @@ const HomepageStyles = styled(motion.div)`
                 margin-top: 0;
                 margin-bottom: 5vh;
             }
-            .link-wrapper{
+            .link-container{
                 overflow: hidden;
                 text-align: right;
                 :nth-child(1){
@@ -105,20 +110,22 @@ const HomepageStyles = styled(motion.div)`
                         padding-right: 0;
                     }
                 }
-                a{
-                    font-size:calc(var(--VW)*7);
-                    line-height: calc(var(--VW)*4.5);
-                    height: calc(var(--VW)*4.5);
+                .link-wrapper{
                     position: relative;
                     top: 15%;
-                    @media (hover: none) and (pointer: coarse), (max-width: 500px){
-                        font-size:calc(var(--VW)*12);
-                        line-height: calc(var(--VW)*11);
-                        height: calc(var(--VW)*11);
-                    }
-                    .number{
-                        pointer-events: none;
-                        font-size:calc(var(--VW)*3);
+                    a{
+                        font-size:calc(var(--VW)*7);
+                        line-height: calc(var(--VW)*4.5);
+                        height: calc(var(--VW)*4.5);
+                        @media (hover: none) and (pointer: coarse), (max-width: 500px){
+                            font-size:calc(var(--VW)*12);
+                            line-height: calc(var(--VW)*11);
+                            height: calc(var(--VW)*11);
+                        }
+                        .number{
+                            pointer-events: none;
+                            font-size:calc(var(--VW)*3);
+                        }
                     }
                 }
             }
@@ -128,6 +135,7 @@ const HomepageStyles = styled(motion.div)`
 
 const Home = ()=>{
     const profile = useRef(null);
+    const {transition, textReveal, opacityReveal} = Animations();
     useEffect(()=>{
             new hoverEffect({
                 parent: profile.current,
@@ -150,39 +158,67 @@ const Home = ()=>{
                 <div className="left-top">
                     <div className="pic" ref={profile}></div>
                     <div className="descriptions">
-                        <p>Award-winning interactive developer striving to craft memorable experiences on the digital canvas. Front-end addicted, Fullstack capable. Currently located in Toronto, Canada.</p>
+                        <motion.p
+                        variants={opacityReveal} initial="initial"
+                        animate="animate"
+                        transition={{...transition, duration:1, delay: 4}}
+                        >Award-winning interactive developer striving to craft memorable experiences on the digital canvas. Front-end addicted, Fullstack capable. Currently located in Toronto, Canada.</motion.p>
                     </div>
                 </div>
                 <div className="left-bottom">
-                        <a href="https://www.linkedin.com/in/ronniefeng/" target="_blank" rel="noreferrer">Li</a>
-                        <a href="https://github.com/lavaboy1738" target="_blank" rel="noreferrer">Gh</a>
+                    <motion.a 
+                    variants={opacityReveal} initial="initial"
+                    animate="animate"
+                    transition={{...transition, duration:1, delay: 5.5}}
+                    href="https://www.linkedin.com/in/ronniefeng/" target="_blank" rel="noreferrer">Li</motion.a>
+                    <motion.a 
+                    variants={opacityReveal} initial="initial"
+                    animate="animate"
+                    transition={{...transition, duration:1, delay: 5.7}}
+                    href="https://github.com/lavaboy1738" target="_blank" rel="noreferrer">Gh</motion.a>
                 </div>
             </div>
             <div className="right">
                 <div className="right-top">
+                    <div className="name-wrapper">
                     <motion.h1
-                    initial={{WebkitTextStrokeColor: "#0f0e0e", WebkitTextFillColor: "#0f0e0e"}}
-                    animate={{WebkitTextStrokeColor: "#0f0e0e", WebkitTextFillColor: "#fac9a0"}}
+                    variants={textReveal} initial="initial"
+                        animate="animate"
+                        transition={{...transition, duration:0.8, delay: 3.5}}
                     >Ronnie</motion.h1>
+                    </div>
+                    <div className="name-wrapper">
                     <motion.h1
-                    initial={{WebkitTextStrokeColor: "#0f0e0e", WebkitTextFillColor: "#0f0e0e"}}
-                    animate={{WebkitTextStrokeColor: "#0f0e0e", WebkitTextFillColor: "#fac9a0"}}
+                    variants={textReveal} initial="initial"
+                        animate="animate"
+                        transition={{...transition, duration:0.8, delay: 3.8}}
                     >Feng</motion.h1>
+                    </div>
                 </div>
                 <div className="right-descriptions">
                     <p>Award-winning interactive developer striving to craft memorable experiences on the digital canvas. Front-end addicted, Fullstack capable. Currently located in Toronto, Canada.</p>
                 </div>
                 <div className="right-bottom">
-                    <motion.div className="link-wrapper"
-                    >
-                        <Link to="works"><span className="number">01.</span>Works</Link>
+                    <motion.div className="link-container">
+                        <motion.div className="link-wrapper"
+                        variants={textReveal} initial="initial" animate="animate" transition= {{...transition, duration: 1, delay: 4.3}}
+                        >
+                            <Link to="works"><span className="number">01.</span>Works</Link>
+                        </motion.div>
                     </motion.div>
-                    <motion.div className="link-wrapper"
-                    >
-                        <Link to="about"><span className="number">02.</span>About</Link>
+                    <motion.div className="link-container">
+                        <motion.div className="link-wrapper"
+                        variants={textReveal} initial="initial" animate="animate" transition= {{...transition, duration: 1, delay: 4.5}}
+                        >
+                            <Link to="about"><span className="number">02.</span>About</Link>
+                        </motion.div>
                     </motion.div>
-                    <motion.div className="link-wrapper">
-                        <a href="mailto:ronnie@ronniefeng.com"><span className="number">03.</span>Contact</a>
+                    <motion.div className="link-container">
+                        <motion.div className="link-wrapper"
+                        variants={textReveal} initial="initial" animate="animate" transition= {{...transition, duration: 1, delay: 4.7}}
+                        >
+                            <a href="mailto:ronnie@ronniefeng.com"><span className="number">03.</span>Contact</a>
+                        </motion.div>
                     </motion.div>
                 </div>
             </div>
